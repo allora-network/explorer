5b6007f45f9321be4f847edf8493ec82e98f6037
conache <C.conache@gmail.com>
2025-02-13 11:09:54 +0100

    Add password protection modal

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
 import { themeChange } from 'theme-change';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import TxDialog from './components/TxDialog.vue';
+import PasswordProtectionModal from './components/PasswordProtectionModa.vue';
+
+const passwordProtectionEnabled =
+  import.meta.env.VITE_PASSWORD_PROTECTION_ENABLED ?? false;
+
+const isAuthenticated = ref(!passwordProtectionEnabled);
 
 onMounted(() => {
   themeChange(false);
+  isAuthenticated.value = !passwordProtectionEnabled;
 });
+
+function handleAuthentication() {
+  isAuthenticated.value = true;
+}
 </script>
 
 <template>
-  <div>
+  <div v-if="isAuthenticated">
     <RouterView />
     <TxDialog />
+  </div>
+  <div v-else>
+    <PasswordProtectionModal @success="handleAuthentication" />
   </div>
 </template>
