250628a93548ee068505a121436e6b03b6460e23
conache <C.conache@gmail.com>
2025-02-13 15:35:44 +0100

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import Layouts from 'vite-plugin-vue-layouts';
@@ -12,8 +12,6 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
   // Get all VITE_ prefixed env variables with dummy values
   const processEnv: Record<string, string> = {
     VITE_PASSWORD_PROTECTION_ENABLED: 'VITE_PASSWORD_PROTECTION_ENABLED',
